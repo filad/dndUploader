469c37b4816b3a36b3dc40e3eda09bd2e8a3945a
Adam Filkor <iconstil@gmail.com>
2012-07-09 16:28:26 -0400

    updated mysql structure

--- external/mysql_connect_sample.php
+++ external/mysql_connect_sample.php
@@ -1,7 +1,7 @@
 <?php
 $db = 'dnduploader';
 $host = 'localhost';
-$user = 'YOUR_HOSTNAME';
+$user = 'YOUR_USERNAME';
 $pass = 'YOUR_PASS';
 mysql_connect($host, $user, $pass) or die('Could not connect to MySQL. Error:' . mysql_error());
 mysql_select_db($db) or die('Could not find Database. Please check if the database information is correct.');		
